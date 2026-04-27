960878524eb6b737e460f2cd22e8429a7a8a79ad
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2026-04-28 00:39:46 +0600

    Update Course.java

--- Course.java
+++ Course.java
@@ -5,22 +5,37 @@ public class Course {
     private String courseName;
     private String department;
     private int credit;
-    private Teacher teacher;
-    private ArrayList<Student> students;
+    private int teacherId;
+    private ArrayList<Integer> studentIds;
 
-    public Course(int courseId, String courseName, String department, int credit, Teacher teacher) {
+    public Course(int courseId, String courseName, String department, int credit, int teacherId) {
         this.courseId = courseId;
         this.courseName = courseName;
         this.department = department;
         this.credit = credit;
-        this.teacher = teacher;
-        students = new ArrayList<>();
+        this.teacherId = teacherId;
+        studentIds = new ArrayList<>();
     }
 
-    public void addStudent(Student s) { students.add(s); }
-    public void removeStudent(Student s) { students.remove(s); }
+    public int getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public String getDepartment() { return department; }
+    public int getCredit() { return credit; }
+    public int getTeacherId() { return teacherId; }
+
+    public void addStudentId(int sid) {
+        if (!studentIds.contains(sid)) studentIds.add(sid);
+    }
+
+    public void removeStudentId(int sid) {
+        studentIds.remove(Integer.valueOf(sid));
+    }
+
+    public ArrayList<Integer> getStudentIds() {
+        return studentIds;
+    }
 
     public String getDetails() {
-        return "Course: " + courseName + " Dept: " + department;
+        return "Course: " + courseName + " | Dept: " + department + " | ID: " + courseId + " | Credit: " + credit + " | TeacherID: " + teacherId;
     }
 }
