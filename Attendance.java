4bcc6b0170891e11b85c6af5db5e0c243a335b88
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2026-04-28 00:04:20 +0600

    Update Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -1,4 +1,3 @@
-// Tracks attendance for a student in a course
 public class Attendance {
     private Student student;
     private Course course;
@@ -9,33 +8,8 @@ public class Attendance {
         this.student = student;
         this.course = course;
         this.totalClasses = totalClasses;
-        this.attendedClasses = 0;
     }
 
-    public Student getStudent() { return student; }
-    public Course getCourse() { return course; }
-    public int getTotalClasses() { return totalClasses; }
-    public int getAttendedClasses() { return attendedClasses; }
-
-    public void markAttendance() {
-        if (attendedClasses < totalClasses) attendedClasses++;
-        else System.out.println("All classes already marked.");
-    }
-
-    public double attendancePercentage() {
-        if (totalClasses == 0) return 0;
-        return (attendedClasses * 100.0) / totalClasses;
-    }
-
-    // Format for file: studentId,courseId,total,attended
-    public String toFileString() {
-        return student.getId() + "," + course.getCourseId() + "," + totalClasses + "," + attendedClasses;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Attendance: %s in %s | %d/%d (%.1f%%)",
-                student.getName(), course.getCourseName(),
-                attendedClasses, totalClasses, attendancePercentage());
-    }
+    public void markAttendance() { attendedClasses++; }
+    public double attendancePercentage() { return (attendedClasses * 100.0) / totalClasses; }
 }
