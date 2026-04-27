63f9a51a405a060a1501bef307c09db357f0cd10
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2026-04-28 00:38:03 +0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,21 +2,30 @@ import java.util.ArrayList;
 
 public class Student extends Person implements Evaluable {
     private double gpa;
-    private ArrayList<Course> enrolledCourses;
+    private ArrayList<Integer> enrolledCourseIds;
 
     public Student(int id, String name, String email, String bloodGroup) {
         super(id, name, email, bloodGroup);
-        enrolledCourses = new ArrayList<>();
+        enrolledCourseIds = new ArrayList<>();
     }
 
-    public void enrollCourse(Course c) { enrolledCourses.add(c); }
-    public void dropCourse(Course c) { enrolledCourses.remove(c); }
+    public void enrollCourseId(int courseId) {
+        if (!enrolledCourseIds.contains(courseId)) enrolledCourseIds.add(courseId);
+    }
 
-    public ArrayList<Course> getEnrolledCourses() { return enrolledCourses; }
+    public void dropCourseId(int courseId) {
+        enrolledCourseIds.remove(Integer.valueOf(courseId));
+    }
 
-    public double calculateGPA() { return gpa; }
+    public ArrayList<Integer> getEnrolledCourseIds() {
+        return enrolledCourseIds;
+    }
+
+    public double calculateGPA() {
+        return gpa;
+    }
 
     public String getDetails() {
-        return "Student: " + getName() + " Email: " + getEmail() + " Blood Group: " + getBloodGroup();
+        return "Student: " + getName() + " | Email: " + getEmail() + " | Blood Group: " + getBloodGroup() + " | ID: " + getId();
     }
 }
