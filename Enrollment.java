1d34fbcd304285b17ab23e8f02383c7ba7d2e0b5
Rahat <86677724+ItzRahatBTW@users.noreply.github.com>
2026-04-28 12:12:49 +0600

    Update Enrollment.java

--- Enrollment.java
+++ Enrollment.java
@@ -4,9 +4,33 @@ public class Enrollment {
     private double grade;
 
     public Enrollment(Student student, Course course) {
-        this.student = student;
-        this.course = course;
+        this.setStudent(student);
+        this.setCourse(course);
     }
 
-    public void updateGrade(double g) { grade = g; }
+    public void updateGrade(double g) { setGrade(g); }
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
 }
